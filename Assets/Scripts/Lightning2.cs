d5cd4750d2f7fb000a3e35791d826e0fef2f1ba4
Igor Pushkar <igor.pushkar@gmail.com>
2016-01-11 19:00:12 +0200

    Added randomized lightning

---
+++ Assets/Scripts/Lightning2.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Lightning2 : MonoBehaviour {
+
+	public Color color = Color.cyan;
+	public float fadeSpeed;
+
+	private int segments;
+	private LineRenderer line;
+	private Vector3 startPoint;
+	private Vector3 endPoint;
+	private float range;
+
+	void Start(){
+		line = GetComponent<LineRenderer> ();
+		line.SetVertexCount (segments);
+		Vector3 vector = endPoint - startPoint;
+
+		for (int i = 0; i < segments; ++i) {
+			Vector3 point = vector / segments * i + startPoint + Random.onUnitSphere * range;
+			line.SetPosition (i, point);
+		}
+
+		line.SetPosition (0, startPoint);
+		line.SetPosition (segments - 1, endPoint);
+	}
+
+	public void SetPoints(Vector3 start, Vector3 end, int segments, float pointRange){
+		startPoint = start;
+		endPoint = end;
+		this.segments = segments;
+		range = pointRange;
+
+	}
+
+	void Update(){
+		color.a -= fadeSpeed * Time.deltaTime;
+
+		line.SetColors (color, color);
+
+		if (color.a <= 0f) {
+			Destroy (this.gameObject);
+		}
+	}
+}
---
+++ Assets/Scripts/LigtningWall2.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LigtningWall2 : MonoBehaviour {
+
+	public GameObject lightning;
+	public int segments;
+	public float damage;
+	public Transform start;
+	public Vector3 startRange;
+	public Transform end;
+	public Vector3 endRange;
+	public float range;
+	public float frequency;
+
+	private BoxCollider boxCollider;
+
+	void OnDrawGizmos(){
+		Gizmos.color = Color.white;
+		Gizmos.DrawWireSphere(start.position, 1);
+		Gizmos.DrawWireSphere(end.position, 1);
+	}
+
+	void Start(){
+		boxCollider = GetComponent<BoxCollider> ();
+		float distance = Vector3.Distance (end.position, start.position);
+		Vector3 direction = end.position - start.position;
+		Vector3 newBoxCenter = direction.normalized * (distance * 0.5f);
+		boxCollider.center += newBoxCenter;
+		Vector3 newBoxSize = direction.normalized * distance;
+		boxCollider.size = newBoxSize;
+		InvokeRepeating ("Spawn", 0.000001f, frequency);
+	}
+
+	void Spawn(){
+		float startX = Random.Range (start.position.x - startRange.x, start.position.x + startRange.x);
+		float startY = Random.Range (start.position.y - startRange.y, start.position.y + startRange.y);
+		float startZ = Random.Range (start.position.z - startRange.z, start.position.z + startRange.z);
+		float endX = Random.Range (end.position.x - endRange.x, end.position.x + endRange.x);
+		float endY = Random.Range (end.position.y - endRange.y, end.position.y + endRange.y);
+		float endZ = Random.Range (end.position.z - endRange.z, end.position.z + endRange.z);
+		Vector3 startPoint = new Vector3 (startX, startY, startZ);
+		Vector3 endPoint = new Vector3 (endX, endY, endZ);
+		GameObject newLightning = Instantiate (lightning, transform.position, Quaternion.identity) as GameObject;
+		newLightning.GetComponent<Lightning2> ().SetPoints (startPoint, endPoint, segments, range);
+	}
+
+//	void LateUpdate(){
+//		if (Time.timeScale > 0) {
+//			Instantiate (lightning, transform.position, Quaternion.identity);
+//		}
+//	}
+
+	void OnTriggerStay(Collider other){
+		if (other.CompareTag ("Robot") || other.CompareTag ("Player")) {
+			other.gameObject.GetComponentInParent<PlayerController> ().ApplyDamage (damage * Time.deltaTime);
+		}
+	}
+}
